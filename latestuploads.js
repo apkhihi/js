bd409e86b3f24f4d9144a41ae7be3059fde8943f
APKHiHi <official.apkhihi@gmail.com>
2017-01-31 21:45:04 +0800

    Update latestuploads.js

--- latestuploads.js
+++ latestuploads.js
@@ -63,7 +63,7 @@ function recent_post_content(){
     if(info_text){
     s += "<div class='wrapper'>";
     s += "<div class='newstitle'>";
-    s += "Latest Uploads";
+    s += "";
     s += "</div>";
     }
     s += "  <marquee style='float:left; margin-left:10px; width:82%' scrollAmount='"+scrolling_speed+"'>";
