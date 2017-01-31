9bcf612763c2635e775a83270e85e53b5b002d03
APKHiHi <official.apkhihi@gmail.com>
2017-01-31 21:49:33 +0800

    Update latestuploads.js

--- latestuploads.js
+++ latestuploads.js
@@ -59,8 +59,13 @@ function recent_post_style(){
     document.write(s);
 }
 function recent_post_content(){
-    var s = "<div id='recent_post'>";
+    var s = "<div id='recent_post' title='Latest Uploads'>";
     if(info_text){
+    s += "<div class='wrapper'>";
+    s += "<div class='newstitle'>";
+    s += "";
+    s += "</div>";
+    }
     s += "  <marquee style='float:left; margin-left:10px; width:82%' scrollAmount='"+scrolling_speed+"'>";
     for(var i=0; i<latest_post; i++){
         var recent_post_entries = entries[i];
